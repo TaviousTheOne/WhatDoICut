598cd21fdf58caffecaf9d9afea5f09c2496449b
TaviousTheOne <samuel.tetreault@gmail.com>
2026-05-26 21:07:13 -0400

    Add summary paragraph and update UI layout

--- app.py
+++ app.py
@@ -90,4 +90,4 @@ def home():
 # Run Flask App
 # ------------------------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=False)
